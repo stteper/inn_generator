3749e352ef27d43af2fbaa81133b1e3e8352a758
Alex Stepanov <stteper@gmail.com>
2021-06-17 21:08:32 +0300

    Fixed email length

--- src/common/email_generator.ts
+++ src/common/email_generator.ts
@@ -6,7 +6,7 @@ class EmailGenerator extends BaseGenerator {
     const part2Symb = digits + lat + lat.toUpperCase() + rus + rus.toUpperCase();
     const part1Symb = part2Symb + '!#&\'*+/^_`';
     const len = Math.max(param && param.len ? param.len : 30, 6);
-    const secPartLen = Math.min(Math.random() * (len - 4) + 1, 62);
+    const secPartLen = Math.min(Math.random() * (len - 5) + 1, 62);
 
     return [
       this.generateRndStr(len - secPartLen - 4, part1Symb),
